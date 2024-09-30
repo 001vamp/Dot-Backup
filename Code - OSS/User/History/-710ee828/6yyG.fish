bd0e06b53601ab0823275807a91d77e5dbc78faa
Jasi <dirtymycup@gmail.com>
2024-09-30 16:16:02 -0400

    system update i rlly like this config

---
+++ Code - OSS/User/History/-710ee828/6yyG.fish
@@ -0,0 +1,29 @@
+#f status is-interactive
+    # Commands to run in interactive sessions can go here
+#end#
+
+#oh-my-posh init fish --config /home/jasi/themes/clean-detailed.omp.json | source
+#
+
+# Commands to run in interactive sessions can go here
+
+# if status --is-interactive
+   # function fish_greeting
+    #    echo "                    .-."
+  #      echo "                     \ \ "
+  #      echo "                      \ \ "
+  #      echo "                       | |"
+  #      echo "                       | |"
+  #      echo "     /\---/\   _,---._ | |"
+  #      echo "    /^   ^  \,'       `. ;"
+  #      echo "   ( O   O   )           ;"
+  #      echo "    `.=o=__,'            \\"
+  #      echo "      /         _,--.__   \\"
+   #     echo "     /  _ )   ,'   `-. `-. \\"
+  #      echo "    / ,' /  ,'        \ \ \ \\"
+   #     echo "   / /  / ,'          (,_)(,_)"
+  #      echo "  (,;  (,,)"
+ #   end
+end#
+pokeget umbreon --hide-name | fastfetch --file-raw -
+oh-my-posh init fish --config /home/jasi/themes/clean-detailed.omp.json | source
